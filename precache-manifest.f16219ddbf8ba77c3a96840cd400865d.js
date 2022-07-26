218544a8e9d08816e488ed50b029a6d6f88afdc1
Yash Khandelwal <yash.khandelwal456@gmail.com>
2022-07-27 01:44:21 +0530

    Updates

---
+++ precache-manifest.f16219ddbf8ba77c3a96840cd400865d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "91a50ab984e31c269be8",
+    "url": "/TextAnalyzer/static/css/main.80be3465.chunk.css"
+  },
+  {
+    "revision": "91a50ab984e31c269be8",
+    "url": "/TextAnalyzer/static/js/main.91a50ab9.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/TextAnalyzer/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/TextAnalyzer/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "9c3cdf6654e1923be7ce",
+    "url": "/TextAnalyzer/static/js/runtime~main.9c3cdf66.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/TextAnalyzer/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "d8c2ff5cc33b39c1922775b1fe9cf4a3",
+    "url": "/TextAnalyzer/index.html"
+  }
+];
